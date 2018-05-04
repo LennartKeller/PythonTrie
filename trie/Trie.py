14e37e2e74e04afe4acdecec692d1ea95628d182
s338298 <RD603km3>
2018-05-04 22:38:36 +0200

    documentation

--- trie/Trie.py
+++ trie/Trie.py
@@ -9,6 +9,11 @@ class Trie:
         self.case_lower = case_lower
 
     def add_word(self, string):
+        """
+        Adds a single word to the trie. If words is already in trie nothing happens.
+        :param string: The word to add
+        :return: None
+        """
         if self.case_lower:
             string = string.lower()
         current_node = self.start_node
@@ -24,11 +29,22 @@ class Trie:
                 if i == len(string) - 1:
                     current_node.word_end = True
 
-    def add_words(self, lst: list):
+    def add_words(self, lst: list[str]):
+        """.
+        Adds all words from a iterable into trie
+        :param lst: The iterable containing the words as strings
+        :return: None
+        """
         for i in lst:
             self.add_word(i)
 
     def check_if_contains(self, string: str):
+        """
+        Checks if the trie contains the given word.
+        These checks have a linear runtime (O(m)) where m is the length of the given word.
+        :param string: word to check
+        :return: True if tries the word, otherwise False
+        """
         if self.case_lower:
             string = string.lower()
         current_node = self.start_node
@@ -40,6 +56,10 @@ class Trie:
         return current_node.word_end
 
     def __len__(self):
+        """
+        Uses breadth-first style algorithm.
+        :return: Number of words in trie
+        """
         counter = 0
         queue = deque((self.start_node.children.values()))
         while len(queue) > 0:
@@ -60,6 +80,9 @@ class Trie:
             if node.children:
                 queue.extendleft(node.children.values())
         return h
+
+    def __iter__(self):
+        pass
 
 
 
